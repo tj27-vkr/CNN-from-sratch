d9e9846feba5f0e59a2b36ef6c7b733f0b625b63
tj27 <tj27-vkr@users.noreply.github.com>
2017-05-20 21:21:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,25 @@ y = np.array([[1],[0]])
 
 arr = cv2.resize(image, (100, 100), interpolation = cv2.INTER_CUBIC)
 
-#convolution
+learning_rate = 0.1
+#error = 0.0
 no_filters = 6
-weights_1 = []
-biases_1 = []
+delta_conv = np.zeros([no_filters, 100,100], dtype = float)
+delta_fc  = np.zeros([no_filters, 100,100], dtype = float)
+loss_conv = np.zeros([no_filters, 100,100], dtype = float)
+weight_1 = np.random.randn(no_filters, 3, 3)
+bias_1 = np.random.randn(no_filters)
+
+weight_2 = np.array([np.random.randn(no_filters*50*50)])
+bias_2 = np.random.randn(number_outputs, 1)
+
+#convolution
 convolved_image = np.empty([no_filters, 100,100], dtype = float)
-
+weights_summation = np.empty([no_filters,1], dtype = float)
 for iteration in range(no_filters):
-    weight_1 = np.random.randn(3, 3)
-    bias_1 = np.random.randn()
-    convolved_image[iteration] = np.array(layer.conv2D(arr, weight_1, bias_1))
-    weights_1.append(weight_1)
-    biases_1.append(bias_1)
-
+    convolved_image[iteration] = np.array(layer.conv2D(arr, weight_1[iteration], bias_1[iteration]))
+    weights_summation[iteration] = np.array(layer.sum_of_weights)
+	
 
 #activation - sigmoid
 activated_values_convolution = layer.activation(convolved_image)
@@ -33,21 +39,34 @@ for i in range(no_filters):
     pooled_image[iteration] = layer.pool(activated_values_convolution[i])
 
 #fully connected
-weight_2 = np.array([np.random.randn(pooled_image.shape[0]*pooled_image.shape[1] \
-                                                *pooled_image.shape[2])])
-bias_2 = np.random.randn(number_outputs, 1)
 fc_output = layer.fullyconnected(pooled_image, weight_2, bias_2)
 
 #activation - sigmoid
 activated_values_fc = layer.activation(fc_output)
 
 #backprop parameters
-loss = layer.loss(y,activated_values_fc)
-delta_fc = loss*layer.activation(activated_values_fc, True)
-#delta_conv = delta_fc
-print (delta_fc.dot(weight_2))
+loss_fc = layer.loss(y,activated_values_fc)
+delta_fc = loss_fc * layer.activation(activated_values_fc, True)
+
+# print (delta_fc)
+# print (weight_2)
+# print ((delta_fc*weight_2).shape)
+# delta_conv = layer.activation(convolved_image, True) * delta_fc * weight_2
+
+'''
+for i in range(no_filters):
+	loss_conv[i] = weights_summation[i] * delta_conv[i]
+#print (loss_conv[0].shape)
+#delta_conv = loss_conv.T.dot(layer.activation(activated_values_convolution, True))
+for i in range(no_filters):
+    delta_conv[i] = loss_conv[i] * layer.activation(activated_values_convolution, True)[0]
+'''
+	
+#updating the weights
+#weight_1 = weight_1 - learning_rate * delta_conv
+weight_2 = weight_2 - learning_rate * delta_fc
 
 #results
 #print ("BackPropogation delta",delta_conv, delta_fc)
-print ("final output:",activated_values_fc)
-print ("error value:",loss)
+#print ("final output:",activated_values_fc)
+print ("error value:",loss_fc)
